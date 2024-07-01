d0526e431750f65a3f3afd4555a225a2e4534253
Ayush Pushpkar <ayushp.ug23.ec@nitp.ac.in>
2024-07-01 23:11:39 +0530

    first commit

--- app/src/main/java/com/example/insidejob/FeaturesActivity.kt
+++ app/src/main/java/com/example/insidejob/FeaturesActivity.kt
@@ -3,11 +3,8 @@ package com.example.insidejob
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
